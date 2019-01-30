c43e3367274081aa918fe2823583a80e99e2ca88
Vasanchai <59070156@it.kmitl.ac.th>
2019-01-31 01:31:57 +0700

    [Weather.py] Change input to input filename from keyboard

--- weather/xmltojson.py
+++ weather/xmltojson.py
@@ -1,28 +1,34 @@
 import json
 import xmltodict
+import os
  
 def xmltojson():
 
-    xmlData = openXMLFile("input")
-    jsonData = convert(xmlData)
-    jsonData = editFormat(jsonData, "\n")
- 
-    createXMLFile(jsonData, "output")
+    xmlData = openXMLFile(input("Enter XML file (Ex. \"input.xml\") >> "))
+
+    if(not xmlData == ""):
+        jsonData = convert(xmlData)
+        jsonData = editFormat(jsonData, "\n")
+        createJSONFile(jsonData, "output")
 
 def openXMLFile(filename):
-    CONST_FILE_TYPE = "xml"
-    with open(filename + "." + CONST_FILE_TYPE, "r") as fileOpen:
-        return fileOpen.read()
+    if (os.path.exists(filename)):
+        with open(filename, "r") as fileOpen:
+            return fileOpen.read()
+    else:
+        print("No such file or directory.")
+        return ""
 
 def convert(xmlData):
     CONST_INDENT = 4
     jsonData = json.dumps(xmltodict.parse(xmlData), indent = CONST_INDENT)
     return jsonData
 
-def createXMLFile(jsonData, filename):
+def createJSONFile(jsonData, filename):
     CONST_FILE_TYPE = "json"
     with open(filename + "." + CONST_FILE_TYPE, "w") as fileOutput:
         fileOutput.write(jsonData)
+        print("Convert XML to JSON Success.")
 
 def editFormat(data, character):
     data = data[0 : data.find('\n')] + data[data.find('\n', data.find('\n') + 2):data.rfind('\n')]
